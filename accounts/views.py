150b5fe744cb85d54d383fe64765e5e5e47f8ba6
leejinwon012 <leejinwon012@naver.com>
2024-04-15 19:39:25 +0900

    회원가입 로그인 로그아웃 구현 메인 홈페이지 연결 필요

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('home')  # 메인 홈페이지에 연결 해야됌
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('home')  # 메인 홈페이지에 연결해야됌
