2bd3c2938bdc5dcafb94d709e11a686ab6800fd6
leejinwon012 <leejinwon012@naver.com>
2024-04-16 00:49:28 +0900

    회원가입 로그인 로그아웃 기능 + 데이터 상속

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+# products/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- spartamarket/settings.py
+++ spartamarket/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'spartamarket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.parent / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
