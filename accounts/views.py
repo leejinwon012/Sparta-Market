9743078d75a7243bd5fd247344cd6da04d7f5f7e
leejinwon012 <leejinwon012@naver.com>
2024-04-17 10:57:27 +0900

    유저 프로필 구현 완료 메인 페이지 연결 전

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from .models import UserProfile, Item
 from django.contrib.auth import login as auth_login, logout as auth_logout
 
 
@@ -28,3 +30,13 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect('home')  # 메인 홈페이지에 연결해야됌
+
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    user_profile, created = UserProfile.objects.get_or_create(user=user)
+    owned_items = Item.objects.filter(owner=user)
+    liked_items = user.liked_items.all()
+    followers_count = user_profile.followers.count()
+    following_count = user.following.count()
+    return render(request, 'profile.html', {'user': user, 'owned_items': owned_items, 'liked_items': liked_items, 'followers_count': followers_count, 'following_count': following_count})
